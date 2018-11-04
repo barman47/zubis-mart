f1d329ab55ba10ee9abc26d77ec394ea33a352d8
barman47 <nomsouzoanya@yahoo.co.uk>
2018-11-04 13:15:15 +0100

    Made updates

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, {'height' : 350, 'indicators' : true});
+    $('.tooltipped').tooltip();
+    $('.parallax').parallax();
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,26 @@
 $(document).ready(function () {
-    $('.dropdown-trigger').dropdown({ hover: true });
+    $('.dropdown-trigger').dropdown({ hover: false });
     $('.sidenav').sidenav();
+
+    window.onscroll = function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById('top').style.display = 'block';
+        } else {
+            document.getElementById('top').style.display = 'none';	
+        }
+    };
+
+    $('#top').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
