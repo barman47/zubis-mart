0b6101604037dd405c346f4f6e8a2752a1474cca
barman47 <nomsouzoanya@yahoo.co.uk>
2019-01-11 16:26:18 +0100

    Added Database Environment Variable

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/zubismart',
-    database: 'mongodb://barman:VICEcity47@ds217360.mlab.com:17360/zubismart',
+    // database: 'mongodb://barman:VICEcity47@ds217360.mlab.com:17360/zubismart',
+    database: process.env.DATABASE_URI || 'mongodb://localhost:27017/zubismart',
     secret: 'barman secret'
 };
