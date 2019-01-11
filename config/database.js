ce1110ba01e8ee76cb4b090770d5986e26f2a21f
barman47 <nomsouzoanya@yahoo.co.uk>
2019-01-11 23:02:47 +0100

    Started Redesigning Homepage

--- config/database.js
+++ config/database.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // database: 'mongodb://localhost:27017/zubismart',
-    // database: 'mongodb://barman:VICEcity47@ds217360.mlab.com:17360/zubismart',
     database: process.env.DATABASE_URI || 'mongodb://localhost:27017/zubismart',
     secret: 'barman secret'
 };
